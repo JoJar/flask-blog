58fd76f5e41130b2fdf7ea5a533c86ada339e30c
Josh Jarvis <jarvisj@cardiff.ac.uk>
2021-01-28 21:16:59 +0000

    begun devloping user/login system in __init__ and modify

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c1635922:differentPassword98@csmysql.cs.cf.ac.uk:3306/c1635922_flask_blog'
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 from blog import routes
